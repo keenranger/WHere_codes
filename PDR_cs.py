9149e966df2d400233cab567080682f333ce1f2f
yu_cs <skdj56@naver.com>
2020-03-19 16:47:25 +0900

    L1test1.2 & 데이터읽는코드 수정

--- PDR_cs.py
+++ PDR_cs.py
@@ -3,16 +3,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
-
-################################################
-#텍스트 파일 읽어오기
-################################################
-f = open("C:/Users/changsoo/PDR_test/Sensor_2020_03_11_01_51_47_50s.txt", 'r')
-All = f.readlines()
-num_row = len(All)  # epoch 개수
-print('num_row : {0}'.format(num_row))
-f.close()
+import sqlite3 as sql
 
 ################################################
 Nan = np.nan
@@ -20,10 +11,31 @@ RtoD = 180 / math.pi
 DtoR = math.pi / 180
 ################################################
 
-sys_time = [0 for i in range(num_row)]  #시스템 시간 data
-acc_data = np.zeros((num_row, 3))       #가속도 data
-gyro_data = np.zeros((num_row, 3))      #자이로 data
 
+################################################
+#텍스트 파일 읽어오기
+################################################
+conn = sql.connect('./0344/database-name.db')
+c= conn.cursor()
+
+
+print("Data parsing... ")
+row_list = np.zeros((1, 16))
+for row in c.execute('SELECT time, accx, accy, accz, linaccx,linaccy, linaccz, gyrox, gyroy, gyroz, magx, magy, magz, yaw, pitch, roll FROM sensordata WHERE filename = "e1" ORDER BY time  '):
+    row_list = np.append(row_list, np.asarray(row).reshape((1, 16)), axis=0)
+
+row_list = row_list[1:, :]
+num_row = len(row_list)
+
+print("Data parsing finished!")
+print("data size : {0}".format(num_row) )
+
+sys_time = row_list[:,0]  #시스템 시간 data
+acc_data = row_list[:,1:4]       #acc data
+lin_acc_data = row_list[:,4:7]       #Linear acc data
+gyro_data = row_list[:,7:10]     #gyro data
+mag_data = row_list[:,10:13]      #Mag data
+ori_data = row_list[:,13:16]     #ori data
 
 norm = [0 for i in range(num_row)]      #가속도 norm data
 smoothing_norm = [0 for i in range(num_row)] #norm smoothing data
@@ -35,15 +47,19 @@ findpeak[0,1] = Nan
 peaktime = np.zeros((num_row-1,2))       #peak가 발생된 시간 check (peaktime[i,0] = peak 가 발생된 시간, peaktime[i,1] = valley 가 발생된 시간
 peakvalley = [0 for i in range(num_row)]  #peak - valley 순서 확인용
 
-
-step_detect = np.zeros((num_row-1,1))    #step detection 알고리즘을 모두 거친 후 찾은 peak data 저장
-
+step_detect = np.zeros((num_row-1,2))    #step detection 알고리즘을 모두 거친 후 찾은 peak data 저장
 step_count = [0 for i in range(num_row)] #step counting
 peak_count = [0 for i in range(num_row)] #peak_counting
 valley_count = [0 for i in range(num_row)] #valley counting
 
 position = np.zeros((num_row, 2))
 ################################################
+
+def cal_norm(data):  #norm 구하기
+    for i in range(0,len(data)):
+        norm[i] = math.sqrt(math.pow(data[i, 0], 2) + math.pow(data[i, 1], 2) + math.pow(data[i, 2], 2))
+
+    return norm
 
 def cal_movmean(data, window_size):  # 이동평균함수 #수정 3/16  np.nean(norm[]) 부분 인덱스
     data_len = len(data)
@@ -58,29 +74,20 @@ def cal_movmean(data, window_size):  # 이동평균함수 #수정 3/16  np.nean(
     return smoothing_norm
 
 ################################################
-for i in range(0, num_row):
-    line = All[i]
-    line = line.split('\t')
 
-    sys_time[i] = line[0]
-    acc_data[i, 0] = line[1]
-    acc_data[i, 1] = line[2]
-    acc_data[i, 2] = line[3]
-
-    gyro_data[i, 0] = line[4]
-    gyro_data[i, 1] = line[5]
-    gyro_data[i, 2] = line[6]
-
-    norm[i] = math.sqrt(math.pow(acc_data[i, 0], 2) + math.pow(acc_data[i, 1], 2) + math.pow(acc_data[i, 2], 2))
-
+norm = cal_norm(acc_data)
 smoothing_norm = cal_movmean(norm,10)
-
 d_norm = np.sign(np.diff(smoothing_norm)) #차분값이기 때문에 길이가 1 작음
 
 
 upper_threshold = 10.35 #peak threshold
 lower_threshold = 9.1   #valley threshold
 
+
+for i in range(0,num_row):
+    gyro_data[i,0] = gyro_data[i,0]* RtoD
+    gyro_data[i,1] = gyro_data[i,1]* RtoD
+    gyro_data[i,2] = gyro_data[i,2]* RtoD
 
 ######################################
 #센서데이터 로깅 100Hz
@@ -134,8 +141,18 @@ for i in range(1,num_row-1) :
 
 
 plt.figure(1)
-plt.plot(peaktime[:,0],c = 'blue')
-plt.plot(peaktime[:,1],c = 'red')
+plt.plot(norm)
+
+# plt.figure(2)
+# plt.plot(gyro_data[:,1],c = 'red')
+#
+# plt.figure(3)
+# plt.plot(gyro_data[:,2],c = 'green')
+
+
+
+# plt.plot(peaktime[:,0],c = 'blue')
+# plt.plot(peaktime[:,1],c = 'red')
 
 print("peaktime_fake_max = {0}".format(np.max(peaktime[:,1])))
 
@@ -168,31 +185,53 @@ for i in range(0,num_row - 1) :
     if ~np.isnan(findpeak[i,1]) :
         peakvalley[i] = -1
 
-
+# plt.figure(2)
+# # plt.plot(norm)
+# plt.plot(smoothing_norm)
+# plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),0],c='red')
+# plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),1],c='green')
 ######################################
 #peak - valley 를 기준으로 step count (peak 다음에는 valley 가 나와야한다)
-
+#추가적인 고민 필요 
 ######################################
-for i in range(0,num_row - 1) :
-    step_detect[i,0] = Nan
+# for i in range(0,num_row - 1) :
+    # step_detect[i,0] = Nan
+    # step_detect[i,1] = Nan
 
 for i in range(1,num_row-1) :
     if peakvalley[i] == -1: #valley 를 먼저 찾고
         for j in range(1,i+1): #이전 값들 중에
             if peakvalley[i-j] == 1: #peak 를 발견하면
-                step_detect[i-j,0] = findpeak[i-j,0] #step_detect 에 그 때의 peak 값을 저장
+                # step_detect[i-j,0] = findpeak[i-j,0] #step_detect 에 그 때의 peak 값을 저장
                 break
-            elif peakvalley[i-j] == -1: #valley 가 발견되면 break
+            elif peakvalley[i-j] == -1: #valley 가 발견되면 두 개 비교 후 업데이트 break
+                if findpeak[i-j,1] > findpeak[i,1] :
+                    findpeak[i-j,1] = Nan
+                else :  findpeak[i,1] = Nan
                 break
+
+for i in range(1,num_row-1) :
+    if peakvalley[i] == 1: #peak 를 먼저 찾고
+        for j in range(1,i+1): #이전 값들 중에
+            if peakvalley[i-j] == -1: #valley 를 발견하면
+                # step_detect[i-j,1] = findpeak[i-j,1] #step_detect 에 그 때의 valley 값을 저장
+                break
+            elif peakvalley[i-j] == 1: #peak 가 발견되면 두 개 비교 후 업데이트 break
+                if findpeak[i-j,0] < findpeak[i,0] :
+                    findpeak[i-j,0] = Nan
+                else :  findpeak[i,0] = Nan
+                break
+
+
+
 
 ######################################
 ######################################
-plt.figure(2)
-# plt.plot(norm)
-plt.plot(smoothing_norm)
-plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),0],c='red')
-plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),1],c='green')
-plt.scatter(range(0,num_row-1),step_detect[range(0,num_row-1),0],c='yellow')
+# plt.figure(3)
+# plt.plot(smoothing_norm)
+# plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),0],c='red')
+# plt.scatter(range(0,num_row-1),findpeak[range(0,num_row-1),1],c='green')
+# plt.scatter(range(0,num_row-1),step_detect[range(0,num_row-1),0],c='yellow')
 
 
 ######################################
@@ -210,18 +249,18 @@ for i in range(0,num_row-1):
     else:
         valley_count[i] = 0
 
-    if ~np.isnan(step_detect[i, 0]):
-        step_count[i] = 1
+    # if ~np.isnan(step_detect[i, 0]):
+    #     step_count[i] = 1
+    #
+    # else:
+    #     step_count[i] = 0
 
-    else:
-        step_count[i] = 0
-
-step = step_count.count(1)
+# step = step_count.count(1)
 peak = peak_count.count(1)
 valley = valley_count.count(1)
 
 # print(acc_data[:, 0])
-print("step counting : {0}".format(step))
+# print("step counting : {0}".format(step))
 print("peak counting : {0}".format(peak))
 print("valley counting : {0}".format(valley))
 
@@ -233,13 +272,15 @@ print("valley counting : {0}".format(valley))
 
 dt = 0.01
 z_angle = [0 for i in range(num_row)]
-
+z_angle_2 = [0 for i in range(num_row)]
 for i in range(1,num_row):
-    z_angle[i] = z_angle[i-1] + gyro_data[i,2]*RtoD*dt
+    z_angle[i] = z_angle[i-1] + gyro_data[i,2]*dt
+    z_angle_2[i]  = z_angle_2[i - 1] + ((gyro_data[i, 2] + gyro_data[i - 1, 2])/2 * dt )
 
 
-# plt.figure(2)
+# plt.figure(4)
 # plt.plot(z_angle)
+# plt.plot(z_angle_2)
 
 #############################################################
 n = peak_count
@@ -251,8 +292,7 @@ for i in range(1,num_row):
     position[i,1] = position[i-1,1] + n[i-1]*L*math.sin(theta[i-1] * DtoR)
 
 
-# plt.figure(3)
+# plt.figure(5)
 # plt.plot(position[range(0,num_row),0],position[range(0,num_row),1])
-
-
 plt.show()
+
