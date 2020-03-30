a8356cd2fead7249b690c51eed3bde3315d13eee
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-03-30 10:47:38 +0900

    큰값으로 교체

--- PeakValleyDetector.py
+++ PeakValleyDetector.py
@@ -8,8 +8,8 @@ class PeakValleyDetector:
         self.step_interval = step_interval
         self.peak_df = pd.DataFrame(columns=("time", "value"))
         self.valley_df = pd.DataFrame(columns=("time", "value"))
-        self.lastPeak = np.array([10000000000000000, 0]) #마지막 피크의 시간과 값이 닮겨있음
-        self.lastValley = np.array([10000000000000000 ,20]) #마지막 밸리의 시간과 값이 닮겨있음
+        self.lastPeak = np.array([np.inf, -np.inf]) #마지막 피크의 시간과 값이 닮겨있음
+        self.lastValley = np.array([np.inf ,np.inf]) #마지막 밸리의 시간과 값이 닮겨있음
         self.acc = np.zeros(3)
         self.finding = 'peak'
         self.norm_df = pd.DataFrame(columns=("time", "value"))
@@ -27,12 +27,12 @@ class PeakValleyDetector:
             if ( (self.current_time - self.lastPeak[0]) > self.step_interval): #시간간격이 충분하면
                 self.peak_df.loc[len(self.peak_df)] = [self.lastPeak[0], self.lastPeak[1]]      
                 self.finding = 'valley'
-                self.lastPeak = [10000000000000000, 0] #시간간격이 부족하거나, 사용했습니다. 비워줘야합니다
+                self.lastPeak = [np.inf, -np.inf] #시간간격이 부족하거나, 사용했습니다. 비워줘야합니다
         else: #밸리를 찾는 중 이라면
             if ( (self.current_time - self.lastValley[0]) > self.step_interval): #시간간격이 충분하면
                 self.valley_df.loc[len(self.valley_df)] = [self.lastValley[0], self.lastValley[1]]
                 self.finding = 'peak'
-                self.lastValley = [10000000000000000, 20] #시간간격이 부족하거나, 사용했습니다. 비워줘야합니다
+                self.lastValley = [np.inf ,np.inf] #시간간격이 부족하거나, 사용했습니다. 비워줘야합니다
 
     def norm_threshold(self): #여기서 norm 값이 threshold 못넘는 값들은 그냥 넘깁니다.
         if (self.euc_norm > self.max_threshold): #피크 threshold를 넘겼을때
