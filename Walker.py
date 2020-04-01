d557a4c2a93c8b485cc64338d869ea84a09f2780
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-04-01 17:44:01 +0900

    walker 객체 생성

---
+++ Walker.py
@@ -0,0 +1,7 @@
+class Walker:
+    def __init__(self, initial_heading = 0, step_length = 70):
+        self.initial_heading = initial_heading
+        self.step_length = step_length
+
+    def step(self, index, time, df1, df2):
+        pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sqlite3 as sql
 import PeakValleyDetector
-
+import Walker
 
 if __name__ == "__main__":
     ## sql을 통해 dataframe으로 db 가져오기
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     print("Data parsing... ")
     query = cur.execute('SELECT time, accx, accy, accz, gyrox, gyroy, gyroz,\
             magx, magy, magz, yaw, pitch, roll FROM sensordata\
-                WHERE filename = "heading1" ORDER BY time')
+                WHERE filename = "heading1" ORDER BY time LIMIT 3000')
     cols = [column[0] for column in query.description]
     sensor_df = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
     conn.close()
@@ -21,10 +21,13 @@ if __name__ == "__main__":
     
     # 피크 밸리 검출
     pvdetect = PeakValleyDetector.PeakValleyDetector()
+    walker = Walker.Walker()
+    
     for index, row in sensor_df[['time', 'accx', 'accy', 'accz']].iterrows():
         if (index % 1000 == 0):
             print("now it`s {0} step.".format(index))
         pvdetect.step(index, row)
+        walker.step(index, row[0], pvdetect.peak_df.tail(1), pvdetect.valley_df.tail(1))
     print(pvdetect.peak_df)
     print(pvdetect.valley_df)
     plt.figure(1)
