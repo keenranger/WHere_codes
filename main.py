9adcb6f48701f5fe88ee293d5e3bad2d60f289ca
yu_cs <skdj56@naver.com>
2020-04-14 18:07:28 +0900

    0414 테스트

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     ## sql을 통해 dataframe으로 db 가져오기
-    file_name = "nh"
-    pvloader = PeakValleyLoader.PeakValleyLoader("./data/0410.db", file_name)
+    file_name = "cc2"
+    pvloader = PeakValleyLoader.PeakValleyLoader("./data/0414.db", file_name)
     sensor_df = pvloader.sensor_df
-    #sensor_df = sensor_df.loc[600:2400]
+    sensor_df['time'] = sensor_df['time'] - sensor_df['time'][0] #처음시간으로 빼줌 #시간 0부터 시작
+    sensor_df = sensor_df.loc[150:]
 
     # 알고리즘엔 쓰이지않고 plot만을 위해 사용되는 부분, 처리속도를 위해 따로 뺌
     norm_df = pd.DataFrame(columns=("time", "value"))
@@ -20,10 +21,15 @@ if __name__ == "__main__":
     pvdetect = PeakValleyDetector.PeakValleyDetector()
 
     plt.figure(1)
-    plt.plot(sensor_df['time'], sensor_df['accx'])
-    plt.plot(sensor_df['time'], sensor_df['accy'])
-    plt.plot(sensor_df['time'], sensor_df['accz'])
-    
+    plt.plot(sensor_df['time'], sensor_df['accx'],c='blue')
+    plt.plot(sensor_df['time'], sensor_df['accy'],c='green')
+    plt.plot(sensor_df['time'], sensor_df['accz'],c='orange')
+    plt.xlabel('time')
+    plt.ylabel('m/s^2')
+
+    plt.figure(2)
+    plt.plot(sensor_df['time'], sensor_df['prox'])
+
     for index, row in sensor_df[['time', 'accx', 'accy', 'accz', 'gyrox', 'gyroy', 'gyroz']].iterrows():
         if (index % 1000 == 0):
             print("now it`s {0} step.".format(index))
