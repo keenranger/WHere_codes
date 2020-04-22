697f612929389f69cf1e884d5fef145253322e58
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-04-22 15:36:13 +0900

    iterrows itertuples로 바꾸기

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from modules import DataLoader
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 if __name__ == "__main__":
     ## sql을 통해 dataframe으로 db 가져오기
-    file_name = "cc2"
-    pvloader = DataLoader.DataLoader("./data/0414.db", file_name)
+    file_name = "heading5"
+    pvloader = DataLoader.DataLoader("./data/headingtest.db", file_name)
     sensor_df = pvloader.sensor_df
     sensor_df['time'] = sensor_df['time'] - sensor_df['time'][0] #처음시간으로 빼줌 #시간 0부터 시작
-    sensor_df = sensor_df.loc[150:]
 
     # 알고리즘엔 쓰이지않고 plot만을 위해 사용되는 부분, 처리속도를 위해 따로 뺌
     norm_df = pd.DataFrame(columns=("time", "value"))
@@ -28,15 +27,17 @@ if __name__ == "__main__":
     plt.xlabel('time')
     plt.ylabel('m/s^2')
 
-    plt.figure(2)
-    plt.plot(sensor_df['time'], sensor_df['prox'])
-
-    for index, row in sensor_df[['time', 'accx', 'accy', 'accz', 'gyrox', 'gyroy', 'gyroz']].iterrows():
-        if (index % 1000 == 0):
-            print("now it`s {0} step.".format(index))
-        pvdetect.step(index, row[:4])
+    for row in sensor_df[['time', 'accx', 'accy', 'accz', 'gyrox', 'gyroy', 'gyroz']].itertuples():
+        if (row[0] % 1000 == 0):
+            print("now it`s {0} step.".format(row[0]))
+        pvdetect.step(row[0], row[1:5])
+    
     pvplotter = PeakValleyPlotter.PeakValleyPlotter(pvdetect, norm_df, file_name)
     print(len(pvdetect.peak_df))
     print(len(pvdetect.valley_df))
+    
+    # for idx, row in pvdetect.peak_df.iterrows():
+    #     if idx >= 5:
+    #         print(row['time'], np.var(np.diff(pvdetect.peak_df.loc[idx-5: idx]['time'])))
     pvplotter.plot()
 
