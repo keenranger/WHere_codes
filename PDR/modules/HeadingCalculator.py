d60caf7ebffadcd702ae39a999761e290598c214
Ulhee <58282484+you-ulhee@users.noreply.github.com>
2020-06-04 17:55:56 +0900

    roll,pitch test data & update HC

--- PDR/modules/HeadingCalculator.py
+++ PDR/modules/HeadingCalculator.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class HeadingCalculator:
     def __init__(self):
         self.acc = [0, 0, 0]
@@ -19,6 +20,7 @@ class HeadingCalculator:
         self.step_count_before = 0
         self.roll = 0
         self.pitch = 0
+        self.yaw = 0
         self.roll_out = 0
         self.pitch_out = 0
         self.RotationX = np.zeros((3, 3))
@@ -28,17 +30,26 @@ class HeadingCalculator:
         self.RtoD = 180 / np.pi
         self.DtoR = np.pi / 180
         self.flag = 0
-        self.windowsize = 20
-        self.avg_value_roll = [] # Moving avg 를 위한 데이터 저장소
-        self.avg_value_pitch = [] # Moving avg 를 위한 데이터 저장소
+        self.windowsize = 10
+        self.avg_value_roll = []  # Moving avg 를 위한 데이터 저장소
+        self.avg_value_pitch = []  # Moving avg 를 위한 데이터 저장소
+        self.pre_avg_roll = 0
+        self.pre_avg_pitch = 0
+        self.pre_avg_data = 0
+        self.index_count = 1
 
     def step(self, idx, row, step_count):  # row[0] : time ,row[1,2,3] = acc 3 axis, row[4,5,6] = gyro 3 axis
         self.time = row[0]
         self.step_count = step_count
         self.acc = [row[1], row[2], row[3]]
         self.gyro = [row[4], row[5], row[6]]
-
         self.cal_heading(self.acc, self.gyro)
+
+        if self.index_count < self.windowsize:
+            self.index_count += 1
+        else:
+            self.index_count = 100
+
         self.time_before = self.time
 
     def cal_heading(self, acc, gyro):  # Calculation heading
@@ -63,8 +74,8 @@ class HeadingCalculator:
             self.step_count_before = self.step_count
 
     def tilting(self, acc):  # Calculation tilting
-        self.pitch = self.Moving_avg("pitch", -np.arctan(acc[0] / np.sqrt(acc[1] ** 2 + acc[2] ** 2)), self.windowsize)
-        self.roll = self.Moving_avg("roll", np.arctan(acc[1] / np.sqrt(acc[0] ** 2 + acc[2] ** 2)), self.windowsize)
+        self.pitch = self.Averaging_F("pitch", -np.arctan(acc[0] / np.sqrt(acc[1] ** 2 + acc[2] ** 2)), self.windowsize)
+        self.roll = self.Averaging_F("roll", np.arctan(acc[1] / np.sqrt(acc[0] ** 2 + acc[2] ** 2)), self.windowsize)
         pass
 
     def Rotation_m(self, roll, pitch, gyro):  # RotationMatrix
@@ -75,9 +86,8 @@ class HeadingCalculator:
         rotation_gyro = np.matmul(self.RotationX, rotation_gyro)
         return rotation_gyro
 
-    def Moving_avg(self, value_name, value, windowsize):
+    def Moving_avg_F(self, value_name, value, windowsize):
         if value_name == 'roll':
-            self.avg_value_roll.append(value)
             value_len = len(self.avg_value_roll)
             if value_len != 0:
                 if value_len <= windowsize:
@@ -97,6 +107,44 @@ class HeadingCalculator:
                     self.avg_value_pitch = self.avg_value_pitch[1:]
                     return sum(self.avg_value_pitch) / windowsize
 
+    def Averaging_F(self, data_name, data, windowsize):
+        if data_name == "roll":
+            if self.index_count == 1:
+                self.pre_avg_roll = data
+                return data
+            elif self.index_count < windowsize:
+                avg_data = self.pre_avg_roll * (self.index_count - 1) / self.index_count + data / self.index_count
+                self.pre_avg_roll = avg_data
+                return avg_data
+            else:
+                avg_data = self.pre_avg_roll * (self.windowsize - 1) / self.windowsize + data / self.windowsize
+                self.pre_avg_roll = avg_data
+                return avg_data
+
+        if data_name == "pitch":
+            if self.index_count == 1:
+                self.pre_avg_pitch = data
+                return data
+            elif self.index_count < windowsize:
+                avg_data = self.pre_avg_pitch * (self.index_count - 1) / self.index_count + data / self.index_count
+                self.pre_avg_pitch = avg_data
+                return avg_data
+            else:
+                avg_data = self.pre_avg_pitch * (windowsize - 1) / windowsize + data / windowsize
+                self.pre_avg_pitch = avg_data
+                return avg_data
+
+    #     if Index == 1
+    #         AVG_Data = Data;
+    #
+    #     elseif
+    #     Index < Win
+    #     AVG_Data = Prev_AvgData * (Index - 1) / Index + Data / Index;
+    #
+    #     else
+    #     AVG_Data = Prev_AvgData * (Win - 1) / Win + Data / Win;
+    #     end
+
     def Heading_plot(self, file_name):
         plt.figure()
         plt.plot(self.heading_df['time'], self.heading_df['value'], c='blue')
@@ -106,4 +154,3 @@ class HeadingCalculator:
         plt.ylabel('degree')
 
         plt.title(file_name)
-
