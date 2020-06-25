fbe0e059282dea2bbc3791fa4a52eb4a1b3fc092
Ulhee <58282484+you-ulhee@users.noreply.github.com>
2020-06-25 10:36:05 +0900

    스윙모션 데이터 추가 및 PitchPVDetection 추가

--- PDR/modules/Walker.py
+++ PDR/modules/Walker.py
@@ -4,26 +4,29 @@ import matplotlib.pyplot as plt
 
 from PDR.modules.PeakValleyDetector import *
 from PDR.modules.HeadingCalculator import *
+from PDR.modules.PitchPVDetector import *
 
 
 class Walker:
     def __init__(self, step_length=0.65):
         self.pvdetect = PeakValleyDetector()
         self.headingcalc = HeadingCalculator()
+        self.pitchpvdetect = PitchPVDetector()
         self.step_length = step_length
         self.peak_cnt_before = np.NaN
         self.pdr_df = pd.DataFrame(
             columns=('length', 'body', 'nav', 'azimuth'))
 
-    def step(self, idx, time, acc, gyro, rot_vec, game_rot_vec):
+    def step(self, idx, time, acc, gyro, rot_vec, game_rot_vec, orientation):
         self.pvdetect.step(idx, time, acc)
+        self.pitchpvdetect.step(idx, time, np.rad2deg(-orientation[1]))
         self.headingcalc.step(time, gyro, rot_vec, game_rot_vec)
 
         peak_cnt = len(self.pvdetect.peak_df)
         if peak_cnt >= 2:  # 피크가 들어온 이후 부터는
             if peak_cnt != self.peak_cnt_before:
-                peak_idx = self.pvdetect.peak_df["idx"].loc[peak_cnt-1]
-                last_peak_idx = self.pvdetect.peak_df["idx"].loc[peak_cnt-2]
+                peak_idx = self.pvdetect.peak_df["idx"].loc[peak_cnt - 1]
+                last_peak_idx = self.pvdetect.peak_df["idx"].loc[peak_cnt - 2]
                 heading_list = mean_angles(
                     self.headingcalc.heading_df.loc[peak_idx], self.headingcalc.heading_df.loc[last_peak_idx])
                 # heading_list = self.headingcalc.heading_df.loc[peak_idx]
@@ -45,7 +48,7 @@ def pdr_to_displacement(pdr_df):
 
 
 def mean_angles(angle_list1, angle_list2):
-    complex_list = np.exp(1j*angle_list1) + np.exp(1j*angle_list2)
+    complex_list = np.exp(1j * angle_list1) + np.exp(1j * angle_list2)
     mean_angle_list = np.angle(complex_list)
     return mean_angle_list
 
