13fd5969a3c1c93b255e31b0c157f92b0306a780
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-06-25 18:33:16 +0900

    같은내용

--- PDR/modules/HeadingCalculator.py
+++ PDR/modules/HeadingCalculator.py
@@ -9,13 +9,16 @@ class HeadingCalculator:
     def __init__(self):
         self.step_before = np.array([np.NaN, np.NaN, np.NaN])
         self.heading_df = pd.DataFrame(
-            columns=("time", "body", "nav", "azimuth"))
+            columns=("time", "body", "nav", "rot", "game"))
 
     def step(self, time, gyro, rot_vec, game_rot_vec):
         # RotationVector를 이용한 Roll, Pitch 계산
-        rotationMatrix = getRotationMatrixFromVector(game_rot_vec, 9)
-        processed_gyro = np.matmul(rotationMatrix, gyro)
+        rotationMatrix = getRotationMatrixFromVector(rot_vec, 9)
         rot_vec_orientation = getOrientation(rotationMatrix)
+        game_vec_orientation = getOrientation(
+            getRotationMatrixFromVector(game_rot_vec, 9))
+
+        processed_gyro = np.matmul(rotationMatrix, gyro)
 
         # 처리된 자이로 적분하면 heading이 나온다
         if not np.isnan(self.step_before[0]):
@@ -33,4 +36,4 @@ class HeadingCalculator:
         self.step_before = [time, gyro[2], processed_gyro[2]]
 
         self.heading_df.loc[len(self.heading_df)] = [
-            time, heading, processed_heading, -rot_vec_orientation[0]]
+            time, heading, processed_heading, -rot_vec_orientation[0], -game_vec_orientation[0]]
