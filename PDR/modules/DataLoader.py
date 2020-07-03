32ddd4c044830115a111bde171afb5a823ff5954
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-07-03 16:29:13 +0900

    loader 통합

--- PDR/modules/DataLoader.py
+++ PDR/modules/DataLoader.py
@@ -3,30 +3,29 @@ import pandas as pd
 
 
 class DataLoader:
-    def __init__(self, file_location, file_name):
-        self.file_location = file_location
+    def __init__(self, file_loc, file_name):
+        self.file_loc = file_loc
         self.file_name = file_name
         self.sensor_df = pd.DataFrame
 
-    def DBLoader(self):  # Dataframe을 읽기위한 부분
-        conn = sql.connect(self.file_location)
-        cur = conn.cursor()
-        print("Data parsing... ")
-        sql_command = 'SELECT * FROM sensordata\
-                          WHERE filename = "' + self.file_name + '" ORDER BY time'
-        query = cur.execute(sql_command)
-        cols = [column[0] for column in query.description]
-        self.sensor_df = pd.DataFrame.from_records(
-            data=query.fetchall(), columns=cols)
-        conn.close()
-        print("Data parsing done!")
+    def loader(self):
+        if self.file_loc[-3:] == "csv" or self.file_loc[-3:] == "txt":
+            print("Data parsing... ")
+            self.sensor_df = pd.read_csv(self.file_loc, names=['experiment', 'time', 'accx', 'accy', 'accz', 'gyrox', 'gyroy',
+                                                               'gyroz', 'magx', 'magy', 'magz', 'rot0', 'rot1', 'rot2', 'rot3', 'game0', 'game1', 'game2', 'game3', 'light'])
+            print("Data parsing done!")
 
-    def TxTLoader(self, seperator=' '):  # textfile을 읽기위한 부분
-        print("Data parsing... ")
-        self.sensor_df = pd.read_csv(self.file_location,
-                                     sep=seperator,
-                                     names=['none', 'time', 'accx', 'accy', 'accz', 'magx', 'magy', 'magz', 'yaw',
-                                            'pitch',
-                                            'roll', 'gyrox', 'gyroy', 'gyroz', 'pres', 'rss1', 'rss2', 'rss3', 'rss4',
-                                            'rss5'])
-        print("Data parsing done!")
+        elif self.file_loc[-2:] == "db":
+            conn = sql.connect(self.file_loc)
+            cur = conn.cursor()
+            print("Data parsing... ")
+            sql_command = 'SELECT * FROM sensordata\
+                            WHERE filename = "' + self.file_name + '" ORDER BY time'
+            query = cur.execute(sql_command)
+            cols = [column[0] for column in query.description]
+            self.sensor_df = pd.DataFrame.from_records(
+                data=query.fetchall(), columns=cols)
+            conn.close()
+            print("Data parsing done!")
+        else:
+            pass
